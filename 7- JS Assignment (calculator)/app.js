fb09b7beccb2ea1b6d38005df6032ef0396e2dbc
Nitesh Kumar <160209872+niteshkumar077@users.noreply.github.com>
2025-03-04 02:01:25 +0500

    Update app.js

--- 7- JS Assignment (calculator)/app.js
+++ 7- JS Assignment (calculator)/app.js
@@ -1,22 +1,22 @@
-let display = document.getElementById("display");
+let display = document.getElementById("display")
 
 function appendInput(value){
-    display.value += value;
+    display.value += value
 }
 
 
 function clearDisplay() {
-    display.value = "";
+    display.value = ""
 }
 
 function deleteLast() {
-    display.value = display.value.slice(0, -1);
+    display.value = display.value.slice(0, -1)
 }
 
 function calculate() {
     try {
-        display.value = eval(display.value);
+        display.value = eval(display.value)
     } catch (e) {
-        display.value = "Error";
+        display.value = "Error"
     }
 }
